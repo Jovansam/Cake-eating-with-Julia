cc83fdc7e310759ea94621e6d5bd0bf635916f08
Jovansam <54563607+Jovansam@users.noreply.github.com>
2020-05-08 10:16:19 +0200

    cake eating

---
+++ cake_eating.jl
@@ -0,0 +1,32 @@
+
+"""
+an illustration of eating a cake of finite and known size
+"""
+
+using JuMP,Ipopt;
+
+function non_renewable_resource()
+
+    A = 1.0;        # chock price
+    B = 0.5*0.5;    # unit cost
+    T = 150;        # final period
+    r = 0.05;       # interest rate
+    S0 = 2.0;       # size of cake
+    γ = 1.5;        # elasticity of substitution
+
+    model = Model(Ipopt.Optimizer)
+    @variable(model, x[t=1:T]>=0)
+    @variable(model, S[t=1:(T+1)]>=0)
+    @constraint(model, [t=1:T], S[t+1] == - x[t] + S[t] )
+    @constraint(model, [t=1], S[t] == 2.0 )
+    @NLobjective(model, Max, sum( ((1+r)^(-t)) * (A*x[t] - B*x[t]^2) for t in 1:T))      # NON-RENEWABLE RESOURCE DEPLETION WITH LINEAR DEMAND
+#    @NLobjective(model, Max, sum( ((1+r)^(-t)) * ((1/(1-γ))*x[t]^(1-γ)) for t in 1:T))  # CAKE EATING WITH ISOELASTIC PREFERENCES
+    JuMP.optimize!(model);
+    x,S=JuMP.value.(x),JuMP.value.(S);
+    S = S[1:T]
+    return(x,S)
+end
+
+x,S = non_renewable_resource()
+
+[S x]
---
+++ readme.md
@@ -0,0 +1,4 @@
+
+# Cake eating
+
+How to eat a cake of finite and know size over time.
